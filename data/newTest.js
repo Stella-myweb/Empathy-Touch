8710dcbfbcb54d99df6e80571f4bf978b37ce230
Stella-myweb <jcl1216@gmail.com>
2024-12-16 14:45:40 +0900

    Create newTest.js

---
+++ data/newTest.js
@@ -0,0 +1,13 @@
+// data/newTest.js
+const newTest = {
+  id: 'new-test',
+  title: '새로운 테스트',
+  thumbnail: 'images/new.jpg',
+  description: '새로운 테스트입니다',
+  questions: [
+    // 문항들...
+  ],
+  results: {
+    // 결과들...
+  }
+};

da8bff881b0d701fedf66e38a3a84da1e4139f02
Stella-myweb <jcl1216@gmail.com>
2024-12-16 14:44:58 +0900

    Create vocabulary.js

---
+++ data/vocabulary.js
@@ -0,0 +1,12 @@
+const vocabularyTest = {
+  id: 'vocabulary',
+  title: '어휘력 테스트',
+  thumbnail: 'images/vocab.jpg',
+  description: '당신의 어휘력은 얼마나 될까요?',
+  questions: [
+    // 문항들...
+  ],
+  results: {
+    // 결과들...
+  }
+};
